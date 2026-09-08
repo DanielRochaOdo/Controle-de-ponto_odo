3c8ee1c26739cd2fe69801a06833132db35434bd
DanielRochaOdo <daniel.rocha@odontoart.com>
2026-09-08 12:15:23 -0300

    style: update app surface colors

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => (
-  <div className="min-h-screen bg-[#f5f8fb] text-slate-900">
+  <div className="min-h-screen bg-[#F8FCF5] text-slate-900">
     <Sidebar />
     <main className="lg:pl-56">
       <div className="mx-auto w-full max-w-[1500px] p-4 sm:p-6 lg:p-8">
